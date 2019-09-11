86d880f365c19a08717baa9092d26f7386d13cd6
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-09-11 15:06:07 -0400

    piggycontroller added

---
+++ Revenge_of_the_Piggies/Assets/_Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform piggy;
+    //if we used just Vector3, we would get just the inital position
+    //transform will update the position value
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 goal = new Vector3(piggy.position.x, transform.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, goal, Time.deltaTime * 5);
+    }
+}
--- Revenge_of_the_Piggies/Assets/_Scripts/CannonController.cs
+++ Revenge_of_the_Piggies/Assets/_Scripts/CannonController.cs
@@ -4,36 +4,50 @@ using UnityEngine;
 
 public class CannonController : MonoBehaviour
 {
-    public GameObject piggyPlayer; //This is a reference to the player object
-    public float strength = 150; //The scalar that defines the strength of the cannon
+    public Rigidbody2D piggyPlayerRB; //This is a reference to the player object
+    const float STRENGTH = 150; //The scalar that defines the strength of the cannon, "const" = constant (does not allow 
+    //vairable to change over time, should be in all caps, right-click to "rename" a variable so that it changes every
+    //instance of the variable) 
+    public Camera mainCamera;
+    Vector3 direction;
+    const float MAX_ANGLE = 90;
+    const float MIN_ANGLE = 0;
 
-    // Start is called before the first frame update
+
     void Start()
     {
 
     }
 
-    // Update is called once per frame
     void Update()
     {
         //get mouse position and direction of the cannon
-        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f);
+        Vector3 mousePosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCamera.transform.position.z);
         //"new" because we have declared an empty variable and need to create it and reserve RAM for it
-        Vector3 mousePositionInWorldCoordinates = Camera.main.ScreenToWorldPoint(mousePosition); //gets coordinates of mouse
-        Vector3 direction = mousePositionInWorldCoordinates - transform.position; //directon for cannon rotation
+        Vector3 mousePositionInWorldCoordinates = mainCamera.ScreenToWorldPoint(mousePosition); //gets coordinates of mouse
+        direction = mousePositionInWorldCoordinates - transform.position; //directon for cannon rotation
 
         //get the rotation of the cannon and have it implemented with mouse movement
         float alpha = Mathf.Acos(Vector3.Dot(Vector3.right, direction.normalized)) * Mathf.Rad2Deg;
         //alpha is the angle of rotation for the cannon
-        //this line returns a radion and we need a degree to input into a quaterion so we use the rad2deg
-        transform.rotation = Quaternion.Euler(new Vector3(0, 0, alpha));
+        //this line returns a radion and we need a degree to input into a quaterion 
+        //so we use the rad2deg
 
-        //fire the cannon upon release of the mouse button
-        if (Input.GetMouseButtonUp(0))
+        if (alpha <= MAX_ANGLE && alpha > MIN_ANGLE && direction.y > 0)
         {
-            piggyPlayer.transform.parent = null;
-            piggyPlayer.GetComponent<Rigidbody2D>().gravityScale = 1;
-            piggyPlayer.GetComponent<Rigidbody2D>().AddForce(direction * strength);
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, alpha));
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        //fire the cannon upon release of the mouse button
+        if (Input.GetButtonUp("Fire 1"))
+        {
+            piggyPlayerRB.transform.parent = null;
+            piggyPlayerRB.gravityScale = 1;
+            piggyPlayerRB.AddForce(direction * STRENGTH *piggyPlayerRB.mass);
         }
     }
 }
