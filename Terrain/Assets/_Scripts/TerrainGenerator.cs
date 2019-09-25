86f34331d80f852736363d3344c0723a7d1e1b96
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-09-25 17:29:24 -0400

    Heightmap/Perlin

---
+++ Terrain/Assets/_Scripts/TerrainGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    Terrain terrain;
+
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        float[,] heightmap = terrain.terrainData.GetHeights(0,0,terrain.terrainData.heightmapWidth, 
+            terrain.terrainData.heightmapWidth);
+
+        for (int i=0; i<terrain.terrainData.heightmapHeight; i++)
+        {
+            for (int j=0; j<terrain.terrainData.heightmapWidth; j++)
+            {
+                float x = j / (float) terrain.terrainData.heightmapWidth;
+                float y = i / (float) terrain.terrainData.heightmapHeight;
+                heightmap[i, j] = Mathf.PerlinNoise(x, y);
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heightmap);
+    }
+
+    void Update()
+    {
+        
+    }
+}
