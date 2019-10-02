78a3208cab77e8ea266e8dff83013e84189a8c2e
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-10-02 17:46:13 -0400

    Oct 2 Class Push

---
+++ CharacterControllerStatePattern/Assets/_Scripts/CharacterControllerStatePattern.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerStatePattern : MonoBehaviour
+{
+    private enum Player_State {S_Walk, S_Idle, S_Run, S_Jump}
+    Player_State state;
+    Animator anim;
+    Rigidbody jump;
+    Vector3 jumpForce;
+ 
+    void Start()
+    {
+        state = Player_State.S_Idle;
+        anim = gameObject.GetComponent<Animator>();
+        jump = GetComponent<Rigidbody>();
+        jumpForce = new Vector3(0f, 5f, 0f);
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state == Player_State.S_Jump)
+        {
+            anim.SetTrigger("walk");
+            state = Player_State.S_Walk;
+        }
+
+    }
+  
+
+    void Update()
+    {
+        switch (state)
+        {
+            case Player_State.S_Idle:
+                if (Input.GetKeyDown(KeyCode.W))
+                {
+                    anim.SetTrigger("walk");
+                    state = Player_State.S_Walk;
+                }
+                break;
+
+            case Player_State.S_Walk:
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    anim.SetTrigger("stop");
+                    state = Player_State.S_Idle;
+                }
+                if (Input.GetKeyDown(KeyCode.LeftShift))
+                {
+                    anim.SetTrigger("run");
+                    state = Player_State.S_Run;
+                }
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    anim.SetTrigger("jump");
+                    state = Player_State.S_Jump;
+                }
+                break;
+
+            case Player_State.S_Run:
+                if (Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    anim.SetTrigger("walk");
+                    state = Player_State.S_Walk;
+                }
+                if (Input.GetKeyUp(KeyCode.W))
+                {
+                    anim.SetTrigger("stop");
+                    state = Player_State.S_Idle;
+                }
+                break;
+
+            case Player_State.S_Jump:
+                jump.AddForce(jumpForce, ForceMode.Force);
+                state = Player_State.S_Walk;
+                break;
+        }
+    }
+}
--- Terrain/Assets/_Scripts/TerrainGenerator.cs
+++ Terrain/Assets/_Scripts/TerrainGenerator.cs
@@ -1,32 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class TerrainGenerator : MonoBehaviour
 {
+    [RangeAttribute(1f,10f)]
+    public float flatness = 1;
+    [RangeAttribute(1f, 20f)]
+    public float frequency = 1;
+    [RangeAttribute(1, 10)]
+    public int octaves = 8;
+    Texture2D image;
     Terrain terrain;
 
     void Start()
     {
         terrain = GetComponent<Terrain>();
-        float[,] heightmap = terrain.terrainData.GetHeights(0,0,terrain.terrainData.heightmapWidth, 
-            terrain.terrainData.heightmapWidth);
-
-        for (int i=0; i<terrain.terrainData.heightmapHeight; i++)
-        {
-            for (int j=0; j<terrain.terrainData.heightmapWidth; j++)
-            {
-                float x = j / (float) terrain.terrainData.heightmapWidth;
-                float y = i / (float) terrain.terrainData.heightmapHeight;
-                heightmap[i, j] = Mathf.PerlinNoise(x, y);
-            }
-        }
-
-        terrain.terrainData.SetHeights(0, 0, heightmap);
+        image = new Texture2D(terrain.terrainData.heightmapWidth, terrain.terrainData.heightmapHeight);
+        image.LoadImage(File.ReadAllBytes("Assets/Mt_Taranaki.png"));
     }
 
     void Update()
     {
-        
+
+        float[,] heightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapWidth,
+            terrain.terrainData.heightmapHeight);
+
+        for (int i = 0; i < terrain.terrainData.heightmapHeight; ++i)
+        {
+            for (int j = 0; j < terrain.terrainData.heightmapWidth; ++j)
+            {
+                float x = j / (float)terrain.terrainData.heightmapWidth;
+                float y = i / (float)terrain.terrainData.heightmapHeight;
+                float height = image.GetPixel(i, j).b; 
+
+                /* Perlin Noise Version
+                float current_frequency = frequency;
+
+                float amplitude = 1f;
+                for (int z = 0; z < octaves; ++z)
+                {
+                    height = height + Mathf.PerlinNoise(x * current_frequency, y * current_frequency) * amplitude;
+                    amplitude /= 2;
+                    current_frequency *= 2;
+                }
+                */
+                heightmap[i, j] = height / flatness + Random.Range(0f, 0.01f);
+            }
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heightmap);
     }
 }
