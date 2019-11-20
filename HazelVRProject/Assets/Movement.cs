74b39d895d99f37f43fcd6cafc7dcbe01e6649de
Hazel Arroyo <hazel.arroyo94@gmail.com>
2019-11-19 23:41:59 -0500

    Added HazelVRProject

---
+++ HazelVRProject/Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VR;
+
+public class Movement : MonoBehaviour
+{
+    //script has teleportation and locomotion in VR
+
+    public Transform centerEye;
+    public float speed = 1f;
+    public float deadZoneThresh = 0.05f;
+
+    private RaycastHit lastRaycastHit;
+    private Transform controllerPosition;
+
+    void Update()
+    {
+        //if (OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).magnitude > deadZoneThresh)
+        {
+            gameObject.transform.position += centerEye.forward * (Time.deltaTime * speed);
+        }
+
+        Physics.Raycast(controllerPosition.position, controllerPosition.transform.forward, out lastRaycastHit);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = lastRaycastHit.point;
+        }
+    }
+
+}
